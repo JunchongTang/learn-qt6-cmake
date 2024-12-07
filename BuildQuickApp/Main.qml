942b6821963f8788153a2c79b270080bcc2e4ac9
Junchong Tang <tjcmail@126.com>
2024-12-08 00:05:38 +0800

    Add build qt quick module project.
    添加一个自定义qt quick模块的cmake示例。

--- BuildQuickApp/Main.qml
+++ BuildQuickApp/Main.qml
@@ -5,4 +5,13 @@ Window {
     height: 480
     visible: true
     title: qsTr("Hello World")
+
+    Item {
+        anchors.fill: parent
+
+        Image {
+            source: ":/qt/qml/assets/github.png"
+            anchors.centerIn: parent
+        }
+    }
 }
--- BuildQuickAppWithCustomModule/main.cpp
+++ BuildQuickAppWithCustomModule/main.cpp
@@ -1,22 +1,18 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-
-    qDebug() << "Import paths:" << engine.importPathList();
-
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreationFailed,
         &app,
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
-    engine.loadFromModule("BuildQuickApp", "Main");
+    engine.loadFromModule("BuildQuickAppWithCustomModule", "Main");
 
     return app.exec();
 }
